a3fab6eb0228356234ba9c4e1024e42111f71281
Stephen Ip <scip@ucsc.edu>
2020-05-12 16:12:47 -0700

    edited Lab3.asm

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -11,6 +11,78 @@
 #
 # Notes:	This program is intended to be run from the MARS IDE.
 ########################################################################################################################################################
+# PSEUDO CODE:
+#
+# prompt user for height:
+#	.data:
+#		prompt
+#	syscall (print string, prompt)
+# 
+# read value from user:
+#	syscall (read integer)
+#	store that value to a temp register ($t0)
+# 	height of pattern = ($t0)
+#
+# if $t0 is less than or equal to 0:
+# 	branch to error condition
+# 	prompt error msg (add error msg prompt to .data)
+# 	return back to main (beginning)
+# 
+# Calculate the number of stars in the first row. = (height - 1) * 2 
+# 	store a value 2 in a register ($t7)
+# 	multiply it by the height - 1 ($t0)
+# 	store num_stars in ($t1)
+# 	restore height to original (height + 1)
+# 
+# Declare counters:
+# 	row counter (1st row suppose to have 1 output, 2nd row 2 outputs etc...) $t2
+# 	number counter left (to keep track of what number we are on) $t3
+# 	output counter (to see if we have outputted the enough number yet) $t4
+# 	number counter left (to keep track of what number we are on) $t5
+#
+# Rows_Loop:
+# 	if height 0: end program
+# 	else:
+# 	increment row count by 1
+# 	start printing left nums
+#
+#
+# Left nums:
+# 	if output counter == row counter: exit loop start printing stars
+# 	else: increment number counter by 1, print it, print tab, increment output counter by 1
+# 	restart loop
+#
+# left_end (pit stop):
+# 	reset output counter back to 0 for next loop
+# 	store the last number to a register ($t5) so it can be subtracted on the right independently without messing up count
+# 	
+# Middle stars:
+# 	if output counter == stars suppose to be output ($t1): exit loop, star printing nums on right
+# 	else:
+# 	print star (character)
+# 	print tab
+# 	increment output counter by 1
+# 	restart loop
+#
+# End_stars:
+# 	decrement number of stars suppose to be output ($t1) by 2 to setup for next row
+# 	increment output counter by 1 for the outputs on the right side so that it finishes one number early and I can manually print the last number to avoid extra tab
+# 	
+# Right nums:
+# 	if output counter == $t2: get ready for next row
+# 	else:
+# 	print number I ended with on the left side ($t5)
+# 	decrement number counter by 1
+# 	increment number outputted by 1
+# 	print tab
+# 	restart loop
+#
+# right_end:
+# 	print last number
+# 	print new line
+# 	reset output counter
+# 	decrement height by 1
+# 	go back to first loop
 
 .text
 main:
@@ -24,9 +96,9 @@ main:
 	
 	ble $t0, $0, Raise_Error   # if height <= 0: raise error message and input height again
 
-	li $s0, 2               # star multiplier is constant (kept in saved register)
+	li $t7, 2               # star multiplier is constant
 	subi $t0, $t0, 1        # (height - 1)
-	mul $t1, $t0, $s0       # number of stars in first row stored in $t1 = [2 * (height - 1)]
+	mul $t1, $t0, $t7       # number of stars in first row stored in $t1 = [2 * (height - 1)]
 	addi $t0, $t0, 1        # restore height to original value
 	
 	li $t2, 0       # row counter 
@@ -37,7 +109,6 @@ main:
 Rows_Loop:
 	beqz $t0, exit_program  # check if height is 0 yet
 	addi $t2, $t2, 1        # increment row count
-	b Left_Hand_Nums_Loop   # begin the process for printing numbers on the left
 	
 Left_Hand_Nums_Loop:
 	beq $t4, $t2, End_Left  # check if the correct amount of numbers have been outputted on the left hand side
